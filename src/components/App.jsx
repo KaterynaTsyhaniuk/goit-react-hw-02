3818629f9576fc9aca7388d5631cb0c9d6348097
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-11-01 15:48:47 +0200

    saved data to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Description from "./Description/Description";
 import Options from "./Options/Options";
 import Feedback from "./Feedback/Feedback";
 import Notification from "./Notification/Notification";
+import feedbackData from "../assets/feedback.json";
 import "modern-normalize";
 import "./App.css";
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [feedback, setFeedback] = useState(
+    () => JSON.parse(localStorage.getItem("feedback")) ?? feedbackData
+  );
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (feedbackType) => {
     setFeedback({
